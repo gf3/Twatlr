ff6d5338b19979bc0c6986be4afee47c3f2f07c0
Gianni Chiappetta <gianni@runlevel6.org>
2011-07-06 17:54:48 -0400

    Initial working commit.

---
+++ tmp.rkt
@@ -0,0 +1,15 @@
+#lang racket
+(require net/url
+         "twatlr.rkt")
+
+(define-values (parent child)
+               (values "88607275937312768" "88617271240568832"))
+
+(displayln (format "get-url:\n~s → ~s" parent (url->string (tweet-url parent))))
+
+(displayln "\n")
+(displayln (format "get-tweet:\n~a" (get-tweet child)))
+
+(displayln "\n")
+(displayln "get-thread:")
+(write (get-thread child))
---
+++ twatlr.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(require net/url
+         (planet dherman/json:3:0))
+
+(define (get-thread tweet-id)
+  (let parent-tweet ([tweet (get-tweet tweet-id)] [a empty])
+    (cond [(eq? #\nul (hash-ref tweet 'in_reply_to_status_id_str)) (append a (list tweet))]
+          [else (parent-tweet (get-tweet (hash-ref tweet 'in_reply_to_status_id_str)) (append a (list tweet)))])))
+
+(define (get-tweet tweet-id)
+  (read-json (get-pure-port (tweet-url tweet-id))))
+
+(define (tweet-url tweet-id)
+  (make-url "http"
+            #f
+            "api.twitter.com"
+            #f
+            #t
+            (list (make-path/param "1" empty)
+                  (make-path/param "statuses" empty)
+                  (make-path/param "show" empty)
+                  (make-path/param (string-append tweet-id ".json") empty))
+            empty
+            #f))
+
+(provide/contract
+  [get-thread (string? . -> . list?)]
+  [get-tweet (string? . -> . hash-eq?)]
+  [tweet-url (string? . -> . url?)])
+
